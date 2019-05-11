3e5951a07fb58dd715cf630524bbedde128e4097
kevin flynn <flynny85@gmail.com>
2019-05-11 15:30:14 +0100

    adding dotenv file setup.

--- app_server/app.js
+++ app_server/app.js
@@ -1,9 +1,15 @@
 "use strict";
 
+// inject env file vars ..
+const dotenv = require('dotenv');
+const envs = dotenv.config({ path: './vars.env' });
+
 // envs 
 const env_name = process.env.npm_package_name;
 const env_version = process.env.npm_package_version;
 const env_mode = (process.env.NODE_ENV).toLowerCase();
+
+
 
 const path = require('path');
 
